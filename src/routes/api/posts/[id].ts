0897f191260427f8f43685db10e842a577b6cd7f
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2024-09-03 10:57:20 -0300

    refactor: use exported post type definitions

--- src/routes/api/posts/[id].ts
+++ src/routes/api/posts/[id].ts
@@ -1,25 +1,18 @@
 import { type APIEvent } from "@solidjs/start/server";
-import { GetTags, Tagged, tagger } from "~/utils/tag";
+import { Post, postId, PostId } from "~/lib/postTypes";
 
-const postId = tagger("PostId");
-type PostId = Tagged<string, GetTags<typeof postId>>;
-
-type Post = {
-  id: PostId;
-  title: string;
-  content: string;
-};
-
-const fetchPost = async (selectedPostId: PostId): Promise<Array<Post>> => {
+const fetchPost = async (
+  selectedPostId: PostId,
+): Promise<Array<Partial<Post> & { content: string }>> => {
   const post = {
     id: selectedPostId,
     title: "Title Lorem Ipsum",
     content: "Lorem ipsum",
-  } satisfies Post;
+  };
   return [post];
 };
 
 export async function GET(event: APIEvent) {
-  const id = postId(event.params.id);
+  const id = postId(Number(event.params.id));
   return fetchPost(id);
 }
