f3fa631fa766b9e1cb6f8af993412cc01978950e
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2024-09-16 14:59:09 -0300

    feat: prettify hover on navigation

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,19 +4,32 @@ export default function Nav() {
   const location = useLocation();
   const active = (path: string) =>
     path == location.pathname
-      ? "transition-all border-primary "
-      : "transition-all border-transparent hover:border-primary";
+      ? "border-primary "
+      : "border-transparent  hover:border-primary";
+
+  const beautifulHover =
+    "hover:text-transparent hover:bg-gradient-to-r hover:from-primary hover:via-primary hover:to-primary/80 hover:bg-clip-text";
   return (
-    <header class="flex items-center bg-background border-b-2 border-tertiary text-foreground/80 font-titillium pb-1 my-2 mx-4">
-      <div class="ml-4 font-montserrat italic text-lg mr-auto font-regular from-transparent to-white text-center items-center">
+    <header
+      role="banner"
+      class="flex items-center bg-background border-b-2 border-tertiary text-foreground/80 font-titillium pb-1 my-2 mx-4"
+    >
+      <a
+        class={`ml-4 font-montserrat italic text-lg mr-auto font-regular from-transparent to-foreground text-center items-center transition-all ease-out ${beautifulHover}`}
+        href="/"
+      >
         ./lucas.d
-      </div>
+      </a>
       <nav class="font-semibold">
-        <ul class="container flex items-center justify-center">
-          <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
+        <ul class="transition-all ease-out container flex items-center justify-center">
+          <li
+            class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6 ${beautifulHover}`}
+          >
             <a href="/">About</a>
           </li>
-          <li class={`border-b-2 ${active("/blog")} mx-1.5 sm:mx-6`}>
+          <li
+            class={`border-b-2 ${active("/blog")} mx-1.5 sm:mx-6 ${beautifulHover}`}
+          >
             <a href="/blog">Blog</a>
           </li>
         </ul>
