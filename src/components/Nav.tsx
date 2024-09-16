a76801148b39c8d51a8f1072d7ac38796b0605f0
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2024-09-16 12:42:38 -0300

    feat: set colors, add logo, and set font

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,18 +4,23 @@ export default function Nav() {
   const location = useLocation();
   const active = (path: string) =>
     path == location.pathname
-      ? "border-sky-600"
-      : "border-transparent hover:border-sky-600";
+      ? "transition-all border-primary "
+      : "transition-all border-transparent hover:border-primary";
   return (
-    <nav class="bg-sky-800">
-      <ul class="container flex items-center p-3 text-gray-200">
-        <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
-          <a href="/">About</a>
-        </li>
-        <li class={`border-b-2 ${active("/blog")} mx-1.5 sm:mx-6`}>
-          <a href="/blog">Blog</a>
-        </li>
-      </ul>
-    </nav>
+    <header class="flex items-center bg-background border-b-2 border-tertiary text-foreground/80 font-titillium pb-1 my-2 mx-4">
+      <div class="ml-4 font-montserrat italic text-lg mr-auto font-regular from-transparent to-white text-center items-center">
+        ./lucas.d
+      </div>
+      <nav class="font-semibold">
+        <ul class="container flex items-center justify-center">
+          <li class={`border-b-2 ${active("/")} mx-1.5 sm:mx-6`}>
+            <a href="/">About</a>
+          </li>
+          <li class={`border-b-2 ${active("/blog")} mx-1.5 sm:mx-6`}>
+            <a href="/blog">Blog</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 }
