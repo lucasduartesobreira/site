3c507ce1246a77c7e0c9616f7473c77d6b9726f9
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2024-09-16 12:35:43 -0300

    feat: add background and foreground color config for tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,21 @@ module.exports = {
   content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        primary: "#7695FF",
+        secondary: "#9DBDFF",
+        tertiary: "#FF9874",
+        /*
+         *background: "#FFD7C4",
+         *foreground: "#374151",
+         */
+        background: "var(hsl(--background))",
+        foreground: "var(hsl(--foreground))",
+      },
+      fontFamily: {
+        montserrat: ["Montserrat", "sans-serif"],
+        titillium: ["Titillium Web", "sans-serif"],
+      },
     },
   },
   plugins: [],
