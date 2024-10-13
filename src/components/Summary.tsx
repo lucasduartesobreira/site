b0f06e1fa44eee67da87fdbb36b248e3e4142c0b
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2024-10-13 15:23:12 -0300

    refactor: make Summary its own component

---
+++ src/components/Summary.tsx
@@ -0,0 +1,53 @@
+export default function Summary() {
+  return (
+    <>
+      <h2 class="text-2xl font-titillium font-bold first:mb-2 text-primary">
+        Summary
+      </h2>
+      <div class="font-regular flex flex-col gap-4 text-left">
+        <p>
+          Driven by a love for problem-solving and competition, I’ve been
+          immersed in software development since 2014. Whether it’s optimizing
+          systems, crafting scalable APIs, or building intuitive interfaces, I
+          thrive on tackling complex challenges with creativity and precision. I
+          find joy in exploring new technologies and continuously improving my
+          craft, from building frameworks to automating workflows and beyond.
+        </p>
+        <p>
+          What defines my work is a dedication to quality and performance, with
+          a focus on clean, efficient code that drives results. Over the years,
+          I’ve worked with a range of technologies—from creating HTTP frameworks
+          in Rust to designing responsive web applications with React and
+          Next.js—always with the goal of delivering elegant and scalable
+          solutions. Every project, whether large or small, is an opportunity to
+          refine my skills and contribute something meaningful.
+        </p>
+        <p>
+          I’m also a strong believer in collaboration, constantly seeking to
+          improve processes and share knowledge within teams. My approach blends
+          technical expertise with a deep love for software development,
+          ensuring that each project reflects not just functionality, but
+          craftsmanship.
+        </p>
+        <div>
+          Key Skills:
+          <ul>
+            <li>
+              Backend: Node.js, TypeScript, Express.js, FastAPI, Rust, NestJS
+            </li>
+            <li>
+              Frontend: React.js, Next.js, Solid.js, Tailwind, Styled Components
+            </li>
+            <li>
+              Databases: MongoDB, PostgreSQL, SQLite AWS: Lambda, S3, SQS, ECS,
+            </li>
+            <li>
+              RedShift, DynamoDB DevOps: Docker, CI/CD, Terraform, GitHub
+              Actions
+            </li>
+          </ul>
+        </div>
+      </div>
+    </>
+  );
+}
