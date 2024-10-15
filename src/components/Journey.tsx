0a2d3b85e9d699b7e05dc9105f6140dd0e2fb926
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2024-10-15 12:16:19 -0300

    feat: create building block to build descriptions

--- src/components/Journey.tsx
+++ src/components/Journey.tsx
@@ -1,31 +1,18 @@
+import { ChevronDown, ChevronUp } from "lucide-solid";
 import {
   children,
   ComponentProps,
+  createContext,
   createMemo,
+  createSignal,
   Match,
   ParentProps,
+  Show,
+  Signal,
+  Suspense,
   Switch,
+  useContext,
 } from "solid-js";
-
-function HighlightSkill(props: ParentProps<ComponentProps<"a">>) {
-  const c = children(() => props.children);
-  const className = () => props.class;
-  const beautifulHover =
-    "hover:text-transparent hover:bg-gradient-to-r hover:from-tertiary hover:via-tertiary hover:to-tertiary50 hover:bg-clip-text";
-
-  const restOfProps = () => {
-    const { class: _cName, children: _children, ...rest } = props;
-    return rest;
-  };
-  return (
-    <a
-      class={`text-primary font-bold font-titillium text-lg ${beautifulHover} ${className()}`}
-      {...restOfProps}
-    >
-      {c()}
-    </a>
-  );
-}
 
 function HighlightParagraph(props: ParentProps<ComponentProps<"p">>) {
   const c = children(() => props.children);
@@ -33,7 +20,7 @@ function HighlightParagraph(props: ParentProps<ComponentProps<"p">>) {
 
   return (
     <p
-      class={`py-2 group-hover/exp:font-semibold transition-all ease-out text-pretty ${className}`}
+      class={`pb-2 group-hover/exp:font-semibold transition-all ease-out text-pretty ${className()}`}
     >
       {c()}
     </p>
@@ -43,7 +30,7 @@ function HighlightParagraph(props: ParentProps<ComponentProps<"p">>) {
 function LeftSideIconContainer(props: ParentProps) {
   const c = children(() => props.children);
   return (
-    <div class="self-center max-w-min min-w-min min-h-min h-full flex justify-center items-center overflow-hidden ">
+    <div class="max-w-min min-w-min min-h-min h-full flex justify-center overflow-hidden ">
       <div class="w-4 h-4 min-w-4 min-h-4 rounded-full border-2 border-tertiary relative group-hover/exp:border-3 group-hover/exp:drop-shadow-[0px_0px_1px_rgba(0,0,0,0.2)] group-hover/exp:bg-tertiary transition-all ease-out">
         {c()}
       </div>
@@ -89,23 +76,75 @@ function LeftSideIconLast() {
 }
 
 function Section(props: ParentProps<{ type: "first" | "middle" | "last" }>) {
-  const c = children(() => props.children);
   const typeOfSection = createMemo(() => props.type);
   return (
-    <section class="flex items-center gap-2 group/exp">
-      <Switch>
-        <Match when={typeOfSection() === "first"}>
-          <LeftSideIconFirst />
-        </Match>
-        <Match when={typeOfSection() === "middle"}>
-          <LeftSideIconMiddle />
-        </Match>
-        <Match when={typeOfSection() === "last"}>
-          <LeftSideIconLast />
-        </Match>
-      </Switch>
-      {c()}
-    </section>
+    <DescriptionContainer>
+      <section class="flex gap-2 group/exp">
+        <Switch>
+          <Match when={typeOfSection() === "first"}>
+            <LeftSideIconFirst />
+          </Match>
+          <Match when={typeOfSection() === "middle"}>
+            <LeftSideIconMiddle />
+          </Match>
+          <Match when={typeOfSection() === "last"}>
+            <LeftSideIconLast />
+          </Match>
+        </Switch>
+        {props.children}
+      </section>
+    </DescriptionContainer>
+  );
+}
+
+const IsDescriptionOpen = createContext<Signal<boolean>>();
+
+function DescriptionContainer(props: ParentProps) {
+  const signal = createSignal(true);
+
+  return (
+    <IsDescriptionOpen.Provider value={signal}>
+      {props.children}
+    </IsDescriptionOpen.Provider>
+  );
+}
+
+function useDescription() {
+  const isOpenContext = useContext(IsDescriptionOpen);
+
+  if (isOpenContext == undefined) {
+    throw "Description is undefined";
+  }
+
+  return isOpenContext;
+}
+
+function Description(props: ParentProps) {
+  const [isOpen] = useDescription();
+  const c = children(() => props.children);
+  return (
+    <div
+      class={`flex gap-1 grow-0 group/description transition-all ease-out duration-1000 ${isOpen() ? "grow-0" : ""}`}
+    >
+      <div class="grow">
+        <Show when={isOpen()}>{c()}</Show>
+      </div>
+    </div>
+  );
+}
+
+function DescriptionButton(props: ParentProps) {
+  const [isOpen, setOpen] = useDescription();
+  return (
+    <button
+      class="flex justify-center items-center"
+      onclick={() => setOpen((open) => !open)}
+    >
+      {props.children}
+      <Show when={isOpen()} fallback={<ChevronDown size={16} />}>
+        <ChevronUp size={16} />
+      </Show>
+    </button>
   );
 }
 
@@ -118,12 +157,31 @@ export default function Journey() {
       <div class="font-regular flex flex-col text-left text-justify text-pretty">
         <Section type="first">
           <HighlightParagraph>
-            Full Stack Developer - Free Lancer - 2023 - Present
+            <DescriptionButton>
+              <span class="flex gap-2 items-center">
+                {"Full Stack Developer - Free Lancer - 2023 - Present"}
+              </span>
+            </DescriptionButton>
+            <Description>
+              Description Description Description Description Description
+              Description Description Description Description Description
+              Description Description Description Description Description
+              Description Description Description Description
+            </Description>
           </HighlightParagraph>
         </Section>
         <Section type="last">
           <HighlightParagraph>
-            Back-end Developer - Free Lancer - 2022 - 2022
+            <span class="flex gap-2">
+              {"Back-end Developer - Free Lancer - 2022 - 2022"}
+              <DescriptionButton></DescriptionButton>
+            </span>
+            <Description>
+              Description Description Description Description Description
+              Description Description Description Description Description
+              Description Description Description Description Description
+              Description Description Description Description
+            </Description>
           </HighlightParagraph>
         </Section>
       </div>
