ed2d9b6bf7ffa6882bd6a577c9726873bee5cefb
Lucas Duarte Sobreira <lucasduartesobreira@gmail.com>
2024-10-20 00:58:08 -0300

    perf: add compression for files bigger than 10 KB

--- app.config.ts
+++ app.config.ts
@@ -1,3 +1,16 @@
 import { defineConfig } from "@solidjs/start/config";
+import compression from "vite-plugin-compression";
 
-export default defineConfig({});
+export default defineConfig({
+  vite: {
+    plugins: [
+      compression({
+        verbose: true,
+        disable: false,
+        threshold: 10240,
+        algorithm: "gzip",
+        ext: ".gz",
+      }),
+    ],
+  },
+});
